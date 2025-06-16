cdf7c4a943a912275ef5b6cf52c2ca0d95989c2a
Felipe Ramos <felipenegrelleramos@gmail.com>
2025-06-16 13:45:55 -0300

    [DEPLOY e SITE] arrumando deploy e add botao de download

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "Deploying to $NGINX_IP"
 
-rsync -avz -e "ssh -p 22" ./index.html "$NGINX_USER"@"$NGINX_IP":/var/www/unitrain-site/index.html
-rsync -avz -e "ssh -p 22" ./logo.png "$NGINX_USER"@"$NGINX_IP":/var/www/unitrain-site/logo.png
-rsync -avz -e "ssh -p 22" ./screenshot.png "$NGINX_USER"@"$NGINX_IP":/var/www/unitrain-site/screenshot.png
+sshpass -p "$NGINX_PASSWORD" rsync -avz -e "ssh -p 22" ./index.html "$NGINX_USER"@"$NGINX_IP":/var/www/unitrain/html/index.html
+sshpass -p "$NGINX_PASSWORD" rsync -avz -e "ssh -p 22" ./logo.png "$NGINX_USER"@"$NGINX_IP":/var/www/unitrain/html/logo.png
+sshpass -p "$NGINX_PASSWORD" rsync -avz -e "ssh -p 22" ./screenshot.png "$NGINX_USER"@"$NGINX_IP":/var/www/unitrain/html/screenshot.png
 
 echo "Deployed!"
